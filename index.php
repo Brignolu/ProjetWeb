4d5adccd79091fe92e0a1bc64ae86b86f68ac2fc
dontcrenie <dontcrenie@yahoo.fr>
2017-11-08 15:28:06 +0100

    Structure Vide
    Structure MVC vide (peut-être à corriger).

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+include "db.php";
+include "model.php";
+include "actions.php";
+include "view.php";
+include "display.php";
+?>
+
+ <!DOCTYPE html>
+<html>
+<head>
+<title>DetteCourseColoc</title>
+</head>
+<body>
+<header>
+</header>
+<h1></h1>
+<p>My first paragraph.</p>
+<footer>
+	
+</footer>
+</body>
+</html> 
---
+++ view.php
@@ -0,0 +1,5 @@
+<?php
+ob_start();
+affiche_dette();
+$contenu = ob_get_clean();
+?>
