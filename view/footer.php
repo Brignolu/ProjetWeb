74af13fda34d7e3a283848d69cac9a3e422ebf74
TheoT <trolle.theo@gmail.com>
2017-12-13 19:13:42 +0100

    commit

---
+++ view/footer.php
@@ -0,0 +1,8 @@
+<footer class="of-container of-footer">
+
+    <h2>Simple, rapide, efficace.</h2>
+    <span>copyright</span>
+
+</footer>
+
+<script type="text/javascript" src="../asset/js/script.js"></script>
