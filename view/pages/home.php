dba738cf149e0ebd42615a0748de56ffc10de55c
TheoTaiTroSek <b.favre.teylaz@gmail.com>
2017-12-20 00:45:32 +0100

    image slider

--- view/pages/home.php
+++ view/pages/home.php
@@ -1,30 +1,14 @@
 <div class="of-container of-home">
-    <h2 class="of-home-title">
-        Accueil
-    </h2>
+    <div class="w3-content w3-section"  >
+        <img class="of-slider" src="asset/img/slider1.jpg"  width="300px" height="350px" >
+        <img class="of-slider" src="asset/img/slider2.jpg"  width="300px" height="350px" >
+        <img class="of-slider" src="asset/img/slider3.jpg"  width="300px" height="350px" >
 
-    <div class="w3-content w3-section" >
-        <img class="of-slider" src="asset/img/slider1.jpg" style="width:100%">
-        <img class="of-slider" src="asset/img/slider2.jpg" style="width:100%">
-        <img class="of-slider" src="asset/img/slider3.jpg" style="width:100%">
-        <img class="of-slider" src="asset/img/slider4.jpg" style="width:100%">
-        <img class="of-slider" src="asset/img/slider5.jpg" style="width:100%">
     </div>
 
-    <h3 class="of-home-subtitle">
 
-    </h3>
 
-    <p class="of-main-corps">
-        Openfive est le plus grand parc de loisirs couvert de France pour enfants de 1 à 12 ans, situé aux portes de Genève ! Insonorisée, climatisée l'été et chauffée l'hiver, magnifiquement décorée sur le thème des pirates, elle comportera plusieurs zones de jeux :
-        - Une baby-gym en mousse pour les bébés de un an:
-        - Un espace dédié pour les enfants de 1 à moins de 4 ans
-        - Un labyrinthe gigantesque pour les enfants de 4 à 12 ans abritant des toboggans géants, des ponts de singe, des canons à balles, un volcan géant et beaucoup d'autres surprises
-        - Un Jump Arena de 16 trampolines
-        - Un hélicoptère à pédales permettant de voyager à 5m de hauteur au dessus du parc
-        - Un accrobranche d'intérieur qui culmine à 8m
-        Toutes ces attractions ont été conçues pour permettre aux parents d'accompagner leurs enfants, accès qui sera autorisé en fonction des pics d'affluence.
-    </p>
+
     <script>
         var myIndex = 0;
         slider();
