8e52707577906aea4cefc8b2bf50bb3a3a39819f
TheoTaiTroSek <b.favre.teylaz@gmail.com>
2017-12-19 23:07:14 +0100

    wesh

--- view/pages/home.php
+++ view/pages/home.php
@@ -7,7 +7,6 @@
         <img class="of-slider" src="asset/img/slider2.jpg" style="width:100%">
         <img class="of-slider" src="asset/img/slider3.jpg" style="width:100%">
         <img class="of-slider" src="asset/img/slider4.jpg" style="width:100%">
-        <img class="of-slider" src="asset/img/slider7.jpg" style="width:100%">
     </div>
 
     <h3 class="of-home-subtitle">
